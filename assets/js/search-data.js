fb953ef393cb625d49833135cea57fe3e511cddf
xmei-hk <128455443+xmei-hk@users.noreply.github.com>
2025-06-26 15:21:30 +0000

    Deploying to gh-pages from @ xmei-hk/xmei-hk.github.io@dabb0456de2d5477683f3155695fb50286f6e5b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,10 +440,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-make-this-page-online",
+            },},{id: "news-make-this-page-online",
           title: 'Make this page online!',
           description: "",
           section: "News",},{id: "projects-project-1",
